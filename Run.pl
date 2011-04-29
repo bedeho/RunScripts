d95ead1301511e1d49b8ab542d4294c94f51dbc0
Bedeho Mender <bedeho.mender@psy.ox.ac.uk>
2011-04-29 12:52:02 +0100

    extended run script and added a plotting script for development

--- Run.pl
+++ Run.pl
@@ -14,18 +14,21 @@
 # Setup
 ########################################################################################
 
-$SLASH = "/"; # Change to \ on windows
+ # Change to \ on windows
 
-#$PROGRAM = "/Network/Servers/mac0.cns.ox.ac.uk/Volumes/Data/Users/mender/Dphil/Projects/VisBack/Model/build.../Release/Model";
-$PROGRAM = "VisBack.exe";
-chdir("d:/Oxford/Work/VisBack/Release");
+# office
+$PROGRAM = "/Network/Servers/mac0.cns.ox.ac.uk/Volumes/Data/Users/mender/Dphil/Projects/VisBack/VisBack/build/Release/VisBack";
+$PROJECTS_FOLDER = "/Network/Servers/mac0.cns.ox.ac.uk/Volumes/Data/Users/mender/Dphil/Projects/"; # must have trailing slash
+$SCRIPT_FOLDER = "/Network/Servers/mac0.cns.ox.ac.uk/Volumes/Data/Users/mender/Dphil/Projects/VisBack/Scripts/VisBackMatlabScripts/";  # must have trailing slash
+$SLASH = "/";
 
-# Must have trailing slash in path
-#$PROJECTS_FOLDER = "/Network/Servers/mac0.cns.ox.ac.uk/Volumes/Data/Users/mender/Dphil/Projects/";
-$PROJECTS_FOLDER = "d:/Oxford/Work/Projects/";
-
-$SCRIPT_FOLDER = "D:/Oxford/Work/Projects/VisBack/VisBackScripts/";
-
+# laptop
+#$PROGRAM = "VisBack.exe";
+#chdir("d:/Oxford/Work/VisBack/Release");
+#$PROJECTS_FOLDER = "d:/Oxford/Work/Projects/";  # must have trailing slash
+#$SCRIPT_FOLDER = "D:/Oxford/Work/Projects/VisBack/VisBackScripts/";  # must have trailing slash
+#$SLASH = "/";
+ 
 $MATLAB = "matlab -nojvm -nodisplay -nosplash ";
 
 ########################################################################################
@@ -51,20 +54,23 @@ $MATLAB = "matlab -nojvm -nodisplay -nosplash ";
 	if($#ARGV >= 1) {
 	        $project = $ARGV[1];
 	} else {
-	        $project = "VisBack";
+	        #$project = "VisBack";
+	        die "No project name provided\n";
 	}
 
 	if($#ARGV >= 2) {
 	        $experiment = $ARGV[2];
 	}
 	else {
-	        $experiment = "Working";
+	        #$experiment = "Working";
+			die "No experiment name provided\n";
 	}
 
 	if($#ARGV >= 3) {
 	        $simulation = $ARGV[3];
 	} else {
-	        $simulation = "20Epoch";
+	        #$simulation = "20Epoch";
+	        die "No simulation name provided\n";
 	}
 
 	$experimentFolder = $PROJECTS_FOLDER.$project.$SLASH."Simulations".$SLASH.$experiment.$SLASH;
@@ -81,22 +87,51 @@ $MATLAB = "matlab -nojvm -nodisplay -nosplash ";
 	        if($command eq "test") {
 
                 	if($#ARGV >= 4) {
-	                        $network = $ARGV[4];
+
+	                        doTest($PROGRAM, $parameterFile, $ARGV[4], $experimentFolder, $simulationFolder);
 	                } else {
-	                        $network = "TrainedNetwork";
+	                        
+	                        # Call doTest() for all files with file name *Network.txt
+	                        # will include
+	                        # 1. trained net
+	                        # 2. intermediate nets
+	                        # 3. untrained control nets
+							opendir(DIR, $simulationFolder) or die $!;
+							
+							while (my $file = readdir(DIR)) {
+							
+								# We only want files
+								next unless (-f "$dir/$file");
+								
+								# Use a regular expression to find files ending in *Network.txt
+								next unless ($file =~ m/Network.txt$/);
+								
+								# Run simulation
+								doTest($PROGRAM, $parameterFile, $file, $experimentFolder, $simulationFolder);
+							}
+							
+							closedir(DIR);	
 	                }
-
-	                $networkFile = $simulationFolder.$network.".txt";
-	                system($PROGRAM." ".$command." ".$parameterFile." ".$networkFile." ".$experimentFolder." ".$simulationFolder);
-
-                        # Do plot of top region
-                        # chdir($SCRIPT_FOLDER);
-                        # $firingRateFile = $simulationFolder."firingRate.dat";
-                        #system($MATLAB . " -r plotRegionHistory('".$firingRateFile."', 5)");
-                        # print $MATLAB . " -r plotRegionHistory('".$firingRateFile."', 5)";
+	                
 	        } elsif($command eq "train") {
+	        	
 	                $networkFile = $experimentFolder."BlankNetwork.txt";
 	                system($PROGRAM." ".$command." ".$parameterFile." ".$networkFile." ".$experimentFolder." ".$simulationFolder);
+	                
+	                # Cleanup
+	                $destinationFolder = $simulationFolder."Training";
+		
+			        if(!(-d $destinationFolder)) {
+			                print "Making result directory...\n";
+			                mkdir($destinationFolder, 0777) || print $!;
+			        }
+					
+					# Move result files into result folder
+			        system("mv ".$simulationFolder."*.dat ".$destinationFolder);
+				
+				    # Move network into result folder
+					system("mv $networkFile ".$destinationFolder);
+	
 	        } elsif ($command eq "loadtest") {
 
 	                # Add md5 test here
@@ -107,20 +142,35 @@ $MATLAB = "matlab -nojvm -nodisplay -nosplash ";
 	                }
 	                system($PROGRAM." ".$command." ".$parameterFile." ".$networkFile." ".$simulationFolder);
 	        }
-
-	        # Cleanup
-	        if($command eq "test") {
-	                $destinationFolder = $simulationFolder.$network;
-	        }
-	        elsif($command eq "train") {
-	                $destinationFolder = $simulationFolder."Training";
-	                # $PROGRAM .= " --silent";
-	        }
-
-	        if(!(-d $destinationFolder)) {
-	                print "Making result directory...\n";
-	                mkdir($destinationFolder, 0777) || print $!;
-	        }
-
-	        system("mv ".$simulationFolder."*.dat ".$destinationFolder);
 	}
+	
+# Run test on network, make result folder
+sub doTest {
+
+	my ($PROGRAM, $parameterFile, $net, $experimentFolder, $simulationFolder) = @_;
+	
+	$networkFile = $simulationFolder.$net;
+	                    
+	system($PROGRAM." test ".$parameterFile." ".$networkFile." ".$experimentFolder." ".$simulationFolder);
+	
+	# Cleanup
+	$newFolder = substr $net, 0, length($net) - 4;
+	$destinationFolder = $simulationFolder.$newFolder;
+	
+   	if(!(-d $destinationFolder)) {
+            print "Making result directory...\n";
+            mkdir($destinationFolder, 0777) || print $!;
+    }
+    
+    # Move result files into result folder
+    system("mv ".$simulationFolder."*.dat ".$destinationFolder);
+    
+    # Move network into result folder
+	system("mv $networkFile ".$destinationFolder);
+	    
+	# Do plot of top region
+	# chdir($SCRIPT_FOLDER);
+	# $firingRateFile = $simulationFolder."firingRate.dat";
+	# system($MATLAB . " -r plotRegionHistory('".$firingRateFile."', 5)");
+	# print $MATLAB . " -r plotRegionHistory('".$firingRateFile."', 5)";     
+}
---
+++ test.pl
@@ -0,0 +1,45 @@
+#!/usr/bin/perl
+
+	use File::Copy;
+
+# COMMAND LINE ARGUMENTS
+# $1: project name : e.g. VisBack
+# $2: experiment name: e.g. Working
+# $3: randomize names (yes/no), default is yes
+
+########################################################################################
+# Setup
+########################################################################################
+
+# office
+$PROJECTS_FOLDER = "/Network/Servers/mac0.cns.ox.ac.uk/Volumes/Data/Users/mender/Dphil/Projects/";  # must have trailing slash
+$PERL_RUN_SCRIPT = "/Network/Servers/mac0.cns.ox.ac.uk/Volumes/Data/Users/mender/Dphil/RunScripts/Run.pl";
+$SLASH = "/";
+
+# laptop
+# $PROJECTS_FOLDER = "D:/Oxford/Work/Projects/";  # must have trailing slash
+# $PERL_RUN_SCRIPT = "C:/MinGW/msys/1.0/home/Mender/Run.pl";
+# $SLASH = "/";
+
+########################################################################################
+
+die "hello";
+
+my $dir = '/Network/Servers/mac0.cns.ox.ac.uk/Volumes/Data/Users/mender/Dphil/Projects/VisBack/Simulations/1Object';
+
+opendir(DIR, $dir) or die $!;
+
+while (my $file = readdir(DIR)) {
+
+   # We only want files
+   next unless (-f "$dir/$file");
+
+   # Use a regular expression to find files ending in .txt
+   next unless ($file =~ m/Network.txt$/);
+   
+   $z = substr $file, 0, length($file) - 4;
+
+   print "$z\n";
+}
+
+closedir(DIR);
